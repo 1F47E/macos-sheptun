a955be374c7fac1cc90e1e9931376734a03b4eac
1F47E <1F47E>
2025-03-22 14:07:43 +0800

    Initial Commit

---
+++ sheptun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sheptun
+//
+//  Created by kass on 22/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sheptun/sheptunApp.swift
@@ -0,0 +1,17 @@
+//
+//  sheptunApp.swift
+//  sheptun
+//
+//  Created by kass on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct sheptunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
