691fb67f5e8f55f69089de827d73c4b5ddd32de8
1F47E <1F47E>
2025-06-18 19:35:53 +0800

    🔖 Update version info with latest git hash and build date
    Update the VersionInfo.swift file with:
        - New git hash: 8d80dff9b23fbdb5b7001b805423cc930ee96a0a
        - Updated build timestamp: 2025-06-18 19:32
    
        This is an automated version update as part of the build process.

--- sheptun/VersionInfo.swift
+++ sheptun/VersionInfo.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct VersionInfo {
-    static let gitHash: String = "1a9a199227ca6137958dc1ca0bf263114dc97e02"
-    static let buildDate: String = "2025-06-18 19:29"
+    static let gitHash: String = "8d80dff9b23fbdb5b7001b805423cc930ee96a0a"
+    static let buildDate: String = "2025-06-18 19:32"
     static let gitBranch: String = "v0.2"
     static let shortHash: String = {
         let hash = gitHash
